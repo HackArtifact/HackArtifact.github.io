3d8f3c39d2211c08201ca6a49199ff729580c879
Brandon Filmer <brandonf2000@hotmail.com>
2022-12-08 16:43:59 +0200

    changed name

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Filmer's Portfolio</title>
+    <link rel="stylesheet" href="./MakeUp/Landing.css"/>
+    <link rel="stylesheet" href="./MakeUp/Top.css"/>
+    <link rel="stylesheet" href="./MakeUp/Bottom.css"/>
+</head>
+<body>
+    <?php include './Head-Foot/nav.php' ?>
+    <!-- <img class="bg-stars" src="./MakeUp/Stars.svg" alt="Stars"> -->
+
+    <!-- Greatings Part -->
+    <h1>
+        
+    </h1>
+
+
+    <!-- About Me -->
+    <img class="business" src="./MakeUp/BusinessPhoto.jpg" alt="Picture of me">
+
+    <?php include './Head-Foot/feet.php' ?>
+</body>
+</html>
